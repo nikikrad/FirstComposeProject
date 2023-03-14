750701805fbbffcc31baaa7e5e17c471804be85d
nikik <nikikrad228@gmail.com>
2023-03-14 23:39:30 +0300

    clear

--- app/src/main/java/com/example/firstcomposeproject/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/firstcomposeproject/ui/home/HomeScreen.kt
@@ -1,42 +1,23 @@
 package com.example.firstcomposeproject.ui.home
 
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.text.Placeholder
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.example.firstcomposeproject.domain.ApiService
 import com.example.firstcomposeproject.domain.response.AnimeResponse
 import com.example.firstcomposeproject.domain.retrofit.RetrofitInstance
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
-import java.lang.Math.ceil
-import java.lang.Math.random
-import kotlin.math.ceil
 import kotlin.random.Random
 
 @OptIn(ExperimentalFoundationApi::class)
