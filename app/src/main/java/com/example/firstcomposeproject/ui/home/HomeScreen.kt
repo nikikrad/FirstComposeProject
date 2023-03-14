3c8b9150a4d57ef97c801a37f3efb6874294a895
nikik <nikikrad228@gmail.com>
2023-03-14 18:38:20 +0300

    clear code

--- app/src/main/java/com/example/firstcomposeproject/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/firstcomposeproject/ui/home/HomeScreen.kt
@@ -2,78 +2,12 @@ package com.example.firstcomposeproject.ui.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.example.firstcomposeproject.navigation.BottomBarScreen
-import com.example.firstcomposeproject.navigation.BottomNavGraph
-import com.example.firstcomposeproject.navigation.Graph
 
 @Composable
 fun HomeScreen() {
    Box(modifier = Modifier.background(Color.Black).fillMaxSize())
-}
-
-@Composable
-fun BottomBar(navController: NavHostController) {
-    val screens = listOf(
-        BottomBarScreen.Home,
-        BottomBarScreen.Search,
-        BottomBarScreen.Favorite,
-        BottomBarScreen.Setting
-    )
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
-
-    BottomNavigation {
-        screens.forEach { screen ->
-            if (currentDestination != null) {
-                AddItem(
-                    screen = screen,
-                    currentDestination = currentDestination,
-                    navController = navController
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun RowScope.AddItem(
-    screen: BottomBarScreen,
-    currentDestination: NavDestination,
-    navController: NavHostController
-) {
-    BottomNavigationItem(
-        label = {
-            Text(text = screen.title)
-        },
-        icon = {
-            Icon(
-                imageVector = screen.icon,
-                contentDescription = "Navigation Icon"
-            )
-        },
-        selected = currentDestination.hierarchy.any {
-            it.route == screen.route
-        },
-        unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
-        onClick = {
-            navController.navigate(screen.route) {
-                popUpTo(navController.graph.findStartDestination().id)
-                launchSingleTop = true
-            }
-        })
 }
